129654f0cb54b8e4ca98cd3925d78fe8b5711c96
ayouknowwho <entheo_alex@mailbox.org>
2023-07-05 11:26:10 +0100

    Fragments added, tab navigation to fragments working.

--- app/src/main/java/com/ayouknowwho/asmrdroid/MainActivity.java
+++ app/src/main/java/com/ayouknowwho/asmrdroid/MainActivity.java
@@ -1,8 +1,11 @@
 package com.ayouknowwho.asmrdroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainerView;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        FragmentContainerView fragmentContainerView = (FragmentContainerView) findViewById(R.id.fragmentContainerView);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab.getPosition() == 0) {
+                    getSupportFragmentManager().beginTransaction()
+                            .setReorderingAllowed(true)
+                            .replace(R.id.fragmentContainerView, HomeFragment.class, null)
+                            .commit();
+                }
+                else if (tab.getPosition() == 1) {
+                    getSupportFragmentManager().beginTransaction()
+                            .setReorderingAllowed(true)
+                            .replace(R.id.fragmentContainerView, ImportFragment.class, null)
+                            .commit();
+                }
+                else if (tab.getPosition() == 2) {
+                    getSupportFragmentManager().beginTransaction()
+                            .setReorderingAllowed(true)
+                            .replace(R.id.fragmentContainerView, GenerateFragment.class, null)
+                            .commit();
+                }
+                else {
+                    /* We shouldn't be able to get here */
+                    getSupportFragmentManager().beginTransaction()
+                            .setReorderingAllowed(true)
+                            .replace(R.id.fragmentContainerView, HomeFragment.class, null)
+                            .commit();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+
+        });
+
     }
 }
